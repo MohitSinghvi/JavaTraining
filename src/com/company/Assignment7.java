14728b59d57b4d078734fb73bd42006a425c532c
MohitSinghvi <msinghvi16@gmail.com>
2020-10-22 10:59:11 +0530

    Dhoni's Batting Position

--- src/com/company/Assignment7.java
+++ src/com/company/Assignment7.java
@@ -1,7 +1,22 @@
 package com.company;
-
+import java.util.*;
 public class Assignment7 {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Enter Current over, current ball, total overs");
+        Scanner sc = new Scanner(System.in);
+        int current_over,current_ball,total_over;
+
+        current_over=sc.nextInt();
+        current_ball=sc.nextInt();
+        total_over=sc.nextInt();
+
+        if((current_over*6+current_ball)<(0.75*(total_over*6))){
+            System.out.println("No");
+        }
+        else{
+            System.out.println("Yes");
+        }
+
+
     }
 }
